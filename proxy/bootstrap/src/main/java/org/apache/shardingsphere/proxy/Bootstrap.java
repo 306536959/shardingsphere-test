812b1fcf827997065c921e77afc71d3e6ad73260
xuao <306536959@qq.com>
2025-01-09 10:21:09 +0800

    feat:通过http请求获取配置文件

--- proxy/bootstrap/src/main/java/org/apache/shardingsphere/proxy/Bootstrap.java
+++ proxy/bootstrap/src/main/java/org/apache/shardingsphere/proxy/Bootstrap.java
@@ -1,11 +1,11 @@
 package org.apache.shardingsphere.proxy;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.shardingsphere.authority.yaml.config.YamlUserConfiguration;
 import org.apache.shardingsphere.infra.config.props.ConfigurationProperties;
 import org.apache.shardingsphere.infra.config.props.ConfigurationPropertyKey;
+import org.apache.shardingsphere.infra.yaml.config.pojo.rule.YamlGlobalRuleConfiguration;
 import org.apache.shardingsphere.proxy.arguments.BootstrapArguments;
 import org.apache.shardingsphere.proxy.backend.config.YamlProxyConfiguration;
 import org.apache.shardingsphere.proxy.backend.config.yaml.YamlProxyDataSourceConfiguration;
@@ -16,11 +16,12 @@ import org.apache.shardingsphere.proxy.frontend.ssl.ProxySSLContext;
 import org.apache.shardingsphere.proxy.initializer.BootstrapInitializer;
 import org.apache.shardingsphere.proxy.initializer.ConfigFetcher;
 import org.apache.shardingsphere.proxy.initializer.ConfigParser;
+
 import org.apache.shardingsphere.proxy.initializer.ConfigWatcher;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -28,7 +29,7 @@ import java.net.ServerSocket;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -42,21 +43,37 @@ public class Bootstrap {
         SLF4JBridgeHandler.install();
     }
 
-    public static void main(final String[] args) throws IOException, SQLException {
+    public static void main(final String[] args) throws IOException, SQLException, ParseException {
         BootstrapArguments bootstrapArgs = new BootstrapArguments(args);
         String configUrl = "https://apifoxmock.com/m2/5550113-5227108-default/250637533";
         YamlProxyConfiguration yamlConfig = loadConfigFromHttp(configUrl);
         YamlProxyDataSourceConfiguration dataSourceConfig = yamlConfig.getDatabaseConfigurations().get("test").getDataSources().get("ds_0");
         YamlProxyServerConfiguration serverConfig = yamlConfig.getServerConfiguration();
+
+//        URL url = new URL("http://localhost:8080/sse");
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setRequestMethod("GET");
+//        connection.setRequestProperty("Accept", "text/event-stream");
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            if (line.startsWith("data:")) {
+//                String data = line.substring(5);
+//                System.out.println("Received message from server: " + data);
+//            }
+//        }
+
+        if (!Objects.isNull(serverConfig.getAuthority())){
         Collection<YamlUserConfiguration> users = serverConfig.getAuthority().getUsers();
         for (YamlUserConfiguration user : users) {
-            log.info("数据库账号:{},数据库密码:{}",user.getUser(),user.getPassword());
+            log.info("数据库账号:{},数据库密码:{}", user.getUser(), user.getPassword());
         }
-        log.info("数据库账号:{},数据库密码:{}",dataSourceConfig.getUsername(),dataSourceConfig.getPassword());
+        log.info("数据库账号:{},数据库密码:{}", dataSourceConfig.getUsername(), dataSourceConfig.getPassword());
+        }
         int port = bootstrapArgs.getPort().orElseGet(() -> new ConfigurationProperties(yamlConfig.getServerConfiguration().getProps()).getValue(ConfigurationPropertyKey.PROXY_DEFAULT_PORT));
         List<String> addresses = bootstrapArgs.getAddresses();
         checkPort(addresses, port);
-
         BootstrapInitializer bootstrapInitializer = new BootstrapInitializer();
         bootstrapInitializer.init(yamlConfig, port, bootstrapArgs.isForce());
 
